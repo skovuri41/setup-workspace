f67eaf6be41178e45768f1fd87f17fb29302b29b
surya <surya46584@gmail.com>
2014-09-29 00:23:21 -0400

    weeknd updates

---
+++ .lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user 
+ { :plugins [[cider/cider-nrepl "0.7.0"]
+            [lein-ancient "0.5.5"]
+            [lein-autoexpect "1.2.2"]
+            [lein-cloverage "1.0.2"]
+            [lein-midje "3.1.3"]]}}
