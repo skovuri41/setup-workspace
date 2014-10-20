923b4b36e184aca4f32e3aaf1ae457c13d8be4a0
surya <surya46584@gmail.com>
2014-10-19 23:18:16 -0400

    drag-stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,7 @@
 (require 'ob)
 (require 'ob-tangle)
 (require 'ob-clojure)
+(require 'cl)
 
 (setq my-emacs-directory (file-name-directory (file-truename load-file-name)))
 (org-babel-load-file (expand-file-name "my-emacs.org" my-emacs-directory))
@@ -34,7 +35,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-bullets-bullet-list (quote ("◉" "○" "►" "✿"))))
+ '(org-bullets-bullet-list (quote ("●" "○" "►" "✿"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
